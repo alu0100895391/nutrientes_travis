f00c3cf9bb1bfd3ef0f14fccd5bf727cbb1c438c
Lucia Ledesma <alu0100895391@ull.edu.es>
2017-10-24 18:10:48 +0100

    Arreglado NoMethodError

--- lib/nutrientes/alimento.rb
+++ lib/nutrientes/alimento.rb
@@ -9,8 +9,15 @@ class Alimento
      @proteinas = proteinas
      @glucidos = glucidos
      @grasas = grasas
+     @valor = 0
   end
+
   def to_s
 	 "#{@nombre}\t#{@proteinas}\t#{@glucidos}\t#{@grasas}"
   end
+
+  def val_energetico
+	@valor = 10*@proteinas+20*@glucidos+9*@grasas
+  end
+
 end
