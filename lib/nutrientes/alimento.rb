5768461db30390253a3d02979490407ae7129b94
Lucia Ledesma <alu0100895391@ull.edu.es>
2017-10-24 17:28:15 +0100

    Arreglado ArgumentError 4

--- lib/nutrientes/alimento.rb
+++ lib/nutrientes/alimento.rb
@@ -2,10 +2,12 @@ class Alimento
  attr_reader :nombre
  attr_reader :proteinas
  attr_reader :glucidos
+ attr_reader :grasas
 
-  def initialize(nombre,proteinas,glucidos)
+  def initialize(nombre,proteinas,glucidos,grasas)
      @nombre = nombre
      @proteinas = proteinas
      @glucidos = glucidos
+     @grasas = grasas
   end
 end
