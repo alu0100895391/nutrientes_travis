3dcc7a9a7711ba52ef19d61101507c08059f268e
Lucia Ledesma <alu0100895391@ull.edu.es>
2017-11-06 16:48:37 +0000

    ArgumentError

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -5,8 +5,23 @@ it "does something useful" do
     expect(false).to eq(false)
   end
 
+context "#Debe existir un nodo y una lista" do
+
+	before :each do 
+		@list = Lista.new(1,8)
+	end
+
+
+
+	it "Debe existir una lista con su cabeza" do
+		expect(@list.head).not_to be nil	
+	end
+	it "Debe existir una lista con su cola" do
+                expect(@list.tail).not_to be nil
+        end
 
 
 
 
+end
 end
