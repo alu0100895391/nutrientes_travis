3fdc3017a1637db0d12d6da2af9fc16420295ddf
Lucia Ledesma <alu0100895391@ull.edu.es>
2017-11-07 17:10:26 +0000

    Extracción del primer alimento de la lista FUNCIONANDO

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -81,7 +81,54 @@ context "#Extracciones" do
 
 end
 
+context "#Comprobaciones con la clase alimento" do
+
+	before :each do
+	@list = Lista.new(nil,nil)
+	@list.insertar( 
+	Tipo_alimento.new("Huevo frito", 14.1,0.0,19.5, "Huevos, lcteos y helados"), 
+        Tipo_alimento.new("Leche vaca", 3.3,4.8,3.2, "Huevos, lacteos y helados"), 
+        Tipo_alimento.new("Yogurt", 3.8, 4.9, 3.8, "Huevos, lacteos y helados"), 
+        Tipo_alimento.new("Cerdo", 21.5, 0.0, 6.3, "Carnes y derivados"), 
+        Tipo_alimento.new("Ternera", 21.1, 0.0, 3.1, "Carnes y derivados"),
+        Tipo_alimento.new("Pollo",20.6, 0.0, 5.6, "Carnes y derivados"), 
+        Tipo_alimento.new("Bacalao",  17.7, 0.0, 0.4, "Pescados y mariscos"), 
+        Tipo_alimento.new("Atun", 21.5, 0.0, 15.5, "Pescados y mariscos"), 
+        Tipo_alimento.new("Salmon",  19.9, 0.0, 13.6, "Pescados y mariscos"), 
+        Tipo_alimento.new("Aceite de oliva", 0.0, 0.2, 99.6, "Alimentos grasos"), 
+        Tipo_alimento.new("Mantequilla", 0.7, 0.0, 83.2, "Alimentos grasos"), 
+        Tipo_alimento.new("Chocolate", 5.3, 47.0, 30.0, "Alimentos grasos"), 
+        Tipo_alimento.new("Azucar", 0.0, 99.8, 0.0, "Alimentos ricos en carbohidratos"), 
+        Tipo_alimento.new("Arroz",6.8, 77.7, 0.6, "Alimentos ricos en carbohidratos"), 
+        Tipo_alimento.new("Lentejas",  23.5, 52.0, 1.4, "Alimentos ricos en carbohidratos"), 
+       Tipo_alimento.new("Papas",2.0, 15.4, 0.1, "Alimentos ricos en carbohidratos"), 
+        Tipo_alimento.new("Tomate", 1.0, 3.5, 0.2, "Verduras y Hortalizas"), 
+        Tipo_alimento.new("Cebolla", 1.3, 5.8, 0.3, "Verduras y Hortalizas"), 
+        Tipo_alimento.new("Calabaza", 1.1, 4.8, 0.1, "Verduras y Hortalizas"), 
+        Tipo_alimento.new("Manzanas", 0.3, 12.4, 0.4, "Frutas"), 
+        Tipo_alimento.new("Platanos", 1.2, 21.4, 0.2, "Frutas"), 
+        Tipo_alimento.new("Pera",  0.5, 12.7, 0.3, "Frutas")
+	)
+end
+	
+
+it "Extraemos el primer alimento" do
+    		 @list.extraer_head
+                expect(@list.head.value.to_s).to eq("Leche vaca\t3.3\t4.8\t3.2")
+
+        end
+
+
+
+
+
+
+
+
 
 
 
 end
+
+end
+
